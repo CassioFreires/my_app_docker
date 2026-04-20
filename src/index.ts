ac7dee2be5cb92cf3b53f619bbb639ff006a1180
Cassio Souza <cassio.souza@ancar.local>
2026-04-20 14:06:53 -0300

    my first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+const porta = process.env.PORT || 3000;
+const message = process.env.MESSAGE || 'Hello, World!';
+
+app.get('/', (req, res) => {
+  res.send(message);
+});
+
+
+
+app.listen(porta, () => {
+  console.log(`Server is running on port ${porta}`);
+});
